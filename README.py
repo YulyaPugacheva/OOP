9fe6a49e714bb56cbbb955bcd46142b6bae49766
YulyaPugacheva <zufarova_yulya@mail.ru>
2026-03-25 11:23:00 +0300

    Update README.py

--- README.py
+++ README.py
@@ -11,47 +11,55 @@ class Student:
         Student.student_list.append(self)
 
     def rate_lect(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
-                if course in lecturer.course_grades:
-                    lecturer.course_grades[course] += [grade]
-                else:
-                    lecturer.course_grades[course] = [grade]
-        else:
-            return 'Ошибка'
+        if (isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress):
+            lecturer.course_grades.setdefault(course, []).append(grade)
+            return lecturer
+        return 'Ошибка'
 
     def __average_hw_grade(self):
+        if len(self.grades) == 0:
+            return None  
         grades_count = 0
         grades_sum = 0
-        for grade in self.grades:
-            grades_count += len(self.grades[grade])
-            grades_sum += sum(self.grades[grade])
-        if grades_count > 0:
-            return grades_sum / grades_count
-        else:
-            return 0
+        for grade_list in self.grades.values():
+            grades_count += len(grade_list)
+            grades_sum += sum(grade_list)
+        return grades_sum / grades_count    
 
     def __lt__(self, other):
         if not isinstance(other, Student):
-            print('Сравниваются объекты разных классов')
-            return
+            return NotImplemented
         return self.__average_hw_grade() < other.__average_hw_grade()
+
 
     def __gt__(self, other):
         if not isinstance(other, Student):
-            print('Сравниваются объекты разных классов')
-            return
+            return NotImplemented
         return self.__average_hw_grade() > other.__average_hw_grade()
 
     def __eq__(self, other):
         if not isinstance(other, Student):
-            print('Сравниваются объекты разных классов')
-            return
+            return NotImplemented
         return self.__average_hw_grade() == other.__average_hw_grade()
 
     def __str__(self):
         average = self.__average_hw_grade()
         some_student = f'Имя: {self.name}\n' f'Фамилия: {self.surname}\n' f'Средняя оценка за домашние задания: {round(average)}\n' f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n' f'Завершенные курсы: {", ".join(self.finished_courses)}\n'
         return some_student
+
+
+
+   # def __average_hw_grade(self):
+        # Находим курсы, по которым есть оценки (ключ существует и список не пуст)
+        #courses_with_grades = [course for course in self.grades if self.grades.get(course)]
+        
+       # if not courses_with_grades:
+            #return 0.0
+
+        #total_sum = sum(sum(self.grades[course]) for course in courses_with_grades)
+       # total_count = sum(len(self.grades[course]) for course in courses_with_grades)
+        
+        #return total_sum / total_count
 
 class Mentor:
     def __init__(self, name, surname):
@@ -68,6 +76,8 @@ class Lecturer(Mentor):
         Lecturer.lecturer_list.append(self)
 
     def __average_lecture_grade(self):
+        if len(self.course_grades) == 0:
+            return None  
         grades_count = 0
         grades_sum = 0
         for grade in self.course_grades:
@@ -80,20 +90,17 @@ class Lecturer(Mentor):
 
     def __lt__(self, other):
         if not isinstance(other, Lecturer):
-            print('Сравниваются объекты разных классов')
-            return
+            return NotImplemented
         return self.__average_lecture_grade() < other.__average_lecture_grade()
 
     def __gt__(self, other):
         if not isinstance(other, Lecturer):
-            print('Сравниваются объекты разных классов')
-            return
+           return NotImplemented
         return self.__average_lecture_grade() > other.__average_lecture_grade()
 
     def __eq__(self, other):
         if not isinstance(other, Lecturer):
-            print('Сравниваются объекты разных классов')
-            return
+            return NotImplemented
         return self.__average_lecture_grade() == other.__average_lecture_grade()
 
     def __str__(self):
@@ -183,20 +190,16 @@ print(best_student1)
 def grades_students(students_list, course):
     overall_student_rating = 0
     lectors = 0
-    for listener in students_list:
-        if course in listener.grades.keys():
-            average_student_score = 0
-            for grades in listener.grades[course]:
-                average_student_score += grades
-            overall_student_rating = average_student_score / len(listener.grades[course])
-            average_student_score += overall_student_rating
+    for student in students_list:
+        if course in student.grades and student.grades[course]:
+            student_avg = sum(student.grades[course]) / len(student.grades[course])
+            overall_student_rating += student_avg
             lectors += 1
-    if overall_student_rating == 0:
+    if lectors== 0:
         return f'Оценок по этому предмету нет'
     else:
         return f'{round(overall_student_rating / lectors, 1)}'
-
-
+    
 def grades_lecturers(lecturer_list, course):
     average_rating = 0
     b = 0
@@ -231,4 +234,6 @@ else:
 print()
 
 print(f'Средняя оценка студентов по курсу "Python": {grades_students(students_list, "Python")}')
+print(f'Средняя оценка лекторов по курсу "Python": {grades_lecturers(lecturer_list, "Python")}')
+students(students_list, "Python")}')
 print(f'Средняя оценка лекторов по курсу "Python": {grades_lecturers(lecturer_list, "Python")}')
